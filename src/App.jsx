bbbcf87c86f01d49ec0344b8357d0b0afc29e6a8
Shekhar Singh <smehla147@gmail.com>
2025-10-02 22:30:14 +1000

    setup the shadcn ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import Login from "./pages/authPages/Login";
+const App = () => {
+  return (
+    <div className="bg-red-600 ">
+      <Login></Login>
+    </div>
+  );
+};
+
+export default App;
